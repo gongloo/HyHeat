1b51b78f98f9174f402eac3160efce730a602453
Hasan Khalil <mian.hasan.khalil@gmail.com>
2025-06-28 19:45:13 -0400

    Update library versions.

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.2.2"
+        #define VERSION "0.2.3"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2024-07-13 01:14:19.774826"
+        #define BUILD_TIMESTAMP "2025-06-28 19:40:44.954410"
     #endif
     
