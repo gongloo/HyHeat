b2a81fe62967a83e35e86cb151d8737b59aea19b
Hasan Khalil <mian.hasan.khalil@gmail.com>
2025-06-28 20:09:42 -0400

    Use ElegantOTA as upload protocol.

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.2.3"
+        #define VERSION "0.2.4"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2025-06-28 19:40:44.954410"
+        #define BUILD_TIMESTAMP "2025-06-28 19:59:45.514796"
     #endif
     
